c1189ea40bf1389d39148f1b6cda83fbb9f65d65
Matt Newsted <mnewsted@illinoislegalaid.org>
2022-10-14 17:06:24 -0500

    removed :icons: from terms

--- setup.py
+++ setup.py
@@ -44,12 +44,12 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ILAO',
-      version='0.0.25',
+      version='1.0.27',
       description=('A docassemble extension.'),
       long_description='# docassemble.ILAO\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Matt Newsted',
-      author_email='formsupport@illinoislegalaid.org',
+      author_email='mnewsted@illinoislegalaid.org',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
       packages=find_packages(),

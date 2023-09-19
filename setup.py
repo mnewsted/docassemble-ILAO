0291d2b635cfd9ed2998f311323beb9698be4b55
Matt Newsted <mnewsted@illinoislegalaid.org>
2023-09-19 17:28:59 -0500

    InterviewListLabelAdjustments

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ILAO',
-      version='1.0.71',
+      version='1.0.72',
       description=('A docassemble extension.'),
       long_description='# docassemble.ILAO\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\nVivian McNaughton, vmcnaughton@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
